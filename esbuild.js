4e11ff7d9b5996563aafe4fc499e6f84b2a2f715
Kwisma <77215384+Kwisma@users.noreply.github.com>
2026-03-23 20:02:55 +0800

    chore: update package version to 1.1.5 and remove external dependencies from esbuild configuration

--- esbuild.js
+++ esbuild.js
@@ -12,8 +12,7 @@ const artifacts = [{ src: 'index.js', dest: 'dist/_worker.js' }];
             target: ['es2022'], // 目标环境
             format: 'esm', // 输出格式 CommonJS
             platform: 'browser', // 目标平台为浏览器
-            logLevel: 'error',
-            external: ['buffer','dns-packet','fastestsmallesttextencoderdecoder']
+            logLevel: 'error'
         });
         console.log(`✔️ 打包完成: ${artifact.src} → ${artifact.dest}`);
     }

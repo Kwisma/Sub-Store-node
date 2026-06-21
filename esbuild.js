0e1c06b4a3c6586ab7d4ba5c741efe3bec6583d3
Kwisma <77215384+Kwisma@users.noreply.github.com>
2026-06-21 21:47:27 +0800

    fix: 修复编译报错 (Closes #2)

--- esbuild.js
+++ esbuild.js
@@ -1,26 +1,55 @@
-import esbuild from 'esbuild';
-import { readFile } from 'fs/promises';
-const pkg = JSON.parse(
-    await readFile(new URL('./src/sub/backend/package.json', import.meta.url))
-);
-const version = pkg.version;
-const artifacts = [{ src: 'index.js', dest: 'dist/_worker.js' }];
-(async () => {
-    for (const artifact of artifacts) {
-        await esbuild.build({
-            entryPoints: [artifact.src], // 入口文件
-            bundle: true, // 启用打包
-            outfile: artifact.dest, // 输出文件
-            sourcemap: true, // 生成 Source Map
-            minify: true, // 压缩代码
-            target: ['es2022'], // 目标环境
-            format: 'esm', // 输出格式 CommonJS
-            platform: 'browser', // 目标平台为浏览器
-            logLevel: 'error',
+
+const fs = require('fs');
+const path = require('path');
+const { build } = require('esbuild');
+
+const objectHasOwnPolyfill = require.resolve('core-js/actual/object/has-own');
+!(async () => {
+    const pkg = JSON.parse(
+        await fs.promises.readFile(
+            './src/sub/backend/package.json',
+            'utf8'
+        )
+    );
+    const version = pkg.version;
+    const mainVersion = JSON.parse(
+        fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'),
+    ).version.trim();
+    const dist = 'dist/_workers.js'
+    const artifacts = [
+        { src: 'index.js', dest: dist },
+    ];
+
+    for await (const artifact of artifacts) {
+        await build({
+            entryPoints: [artifact.src],
+            bundle: true,
+            minify: true,
+            sourcemap: false,
+            platform: 'browser',
+            format: 'esm',
+            outfile: artifact.dest,
+            inject: [objectHasOwnPolyfill],
             define: {
                 __VERSION__: `"${version}"`
             }
         });
+        fs.writeFileSync(
+            path.join(__dirname, dist),
+            `// SUB_STORE_NODE_VERSION: ${mainVersion}
+${fs.readFileSync(path.join(__dirname, dist), {
+                encoding: 'utf8',
+            })}`,
+            {
+                encoding: 'utf8',
+            },
+        );
         console.log(`✔️ 打包完成: ${artifact.src} → ${artifact.dest}`);
     }
-})();
+})()
+    .catch((e) => {
+        console.log(e);
+    })
+    .finally(() => {
+        console.log('done');
+    });
